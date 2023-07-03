07c31f7db34f39a7152c3fdc252d05ad601d3fda
wenshiu.hsu <wenshiu.hsu@gmail.com>
2023-07-03 15:40:35 +0800

    refactor: 💡 main function
    Split main into functions and execute them separately according to the
    steps

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const rankJsonPath = "./rank.json";
 // leader board table readme path
 const markdownPath = "./test.md";
 const displayContributorCounts = 10;
+const markdownInsertKey = "CONTRIBUTION-LEADER-BOARD-TABLE";
 
+// utility function
 const stringTag = options => {
     const attrs = options.attrs ?? {};
     const attrsString = Object.keys(attrs)
@@ -27,63 +29,83 @@ const stringTag = options => {
     return `<${options.tag} ${attrsString} />`;
 };
 
+async function getLeaderBoard() {
+    const leaderBoard = new LeaderBoard(rankJsonPath);
+    await leaderBoard.init();
+    const lastUpdateTime = leaderBoard.Rank.lastUpdateTime;
+    if (lastUpdateTime === null) {
+        core.info(`Because the ${rankJsonPath} file does not exist, it is not possible to compare past rankings, so everyone's last rank is displayed as '-'`);
+    }
+
+    return {
+        leaderBoard,
+        lastUpdateTime,
+    };
+}
+
+function generateMDTable({ leaderBoard, lastUpdateTime }) {
+    const leaderBoardMd = jsonToMdTable(
+        // get top X contributors
+        leaderBoard.getLeaders(displayContributorCounts).map((data, index) => {
+            const rank = index + 1;
+            const lastRank = (() => {
+                if (lastUpdateTime === null) return "-";
+
+                if (data?.lastRank) {
+                    const rankChange = data.lastRank - rank;
+                    if (rankChange < 0) {
+                        return `${stringTag({ tag: "font", attrs: { color: "red" }, content: "⇩" })} ${rankChange * -1}`;
+                    }
+                    if (rankChange > 0) {
+                        return `${stringTag({ tag: "font", attrs: { color: "green" }, content: "⇧" })} ${rankChange}`;
+                    }
+                    return "-";
+                }
+
+                return "New Contributor!";
+            })();
+            const name = `${stringTag({ tag: "img", attrs: { width: "30px", src: data.info.avatar_url, alt: data.info.login } })} [${data.info.login}](${data.info.html_url})`;
+            return {
+                Rank: rank,
+                Name: name,
+                Score: data.totalScore,
+                "Last Rank": lastRank,
+            };
+        }),
+        ["Rank", "Name", "Score", "Last Rank"]
+    );
+
+    return { leaderBoardMd };
+}
+
+function writeMD({ leaderBoardMd }) {
+    const currentReadme = Utils.markdown.read(markdownPath);
+    const newReadme = Utils.markdown.insert(currentReadme, leaderBoardMd + `\nUpdate Time: ${dayjs().utc().format("YYYY/MM/DD HH:mm:ss ZZ")}`, markdownInsertKey);
+    if (newReadme !== currentReadme) {
+        core.info("Writing to " + markdownPath);
+        Utils.markdown.write(markdownPath, newReadme);
+    } else {
+        core.info("No need to update readme");
+    }
+    return {};
+}
+
 // most @actions toolkit packages have async methods
-async function run() {
+async function main() {
     try {
         // Get leaderBoard
-        const leaderBoard = new LeaderBoard(rankJsonPath);
-        await leaderBoard.init();
-        const lastUpdateTime = leaderBoard.Rank.lastUpdateTime;
-        if (lastUpdateTime === null) {
-            core.info(`Because the ${rankJsonPath} file does not exist, it is not possible to compare past rankings, so everyone's last rank is displayed as '-'`);
-        }
+        const { leaderBoard, lastUpdateTime } = await getLeaderBoard();
 
         // Generate markdown string
-        const leaderBoardMd = jsonToMdTable(
-            // get top X contributors
-            leaderBoard.getLeaders(displayContributorCounts).map((data, index) => {
-                const rank = index + 1;
-                const lastRank = (() => {
-                    if (lastUpdateTime === null) return "-";
-
-                    if (data?.lastRank) {
-                        const rankChange = data.lastRank - rank;
-                        if (rankChange < 0) {
-                            return `${stringTag({ tag: "font", attrs: { color: "red" }, content: "⇩" })} ${rankChange * -1}`;
-                        }
-                        if (rankChange > 0) {
-                            return `${stringTag({ tag: "font", attrs: { color: "green" }, content: "⇧" })} ${rankChange}`;
-                        }
-                        return "-";
-                    }
-
-                    return "New Contributor!";
-                })();
-                const name = `${stringTag({ tag: "img", attrs: { width: "30px", src: data.info.avatar_url, alt: data.info.login } })} [${data.info.login}](${data.info.html_url})`;
-                return {
-                    Rank: rank,
-                    Name: name,
-                    Score: data.totalScore,
-                    "Last Rank": lastRank,
-                };
-            }),
-            ["Rank", "Name", "Score", "Last Rank"]
-        );
+        const { leaderBoardMd } = generateMDTable({ leaderBoard, lastUpdateTime });
 
         // Write markdown
-        const currentReadme = Utils.markdown.read(markdownPath);
-        const newReadme = Utils.markdown.insert(currentReadme, leaderBoardMd + `\nUpdate Time: ${dayjs().utc().format("YYYY/MM/DD HH:mm:ss ZZ")}`, "CONTRIBUTION-LEADER-BOARD-TABLE");
-        if (newReadme !== currentReadme) {
-            core.info("Writing to " + markdownPath);
-            Utils.markdown.write(markdownPath, newReadme);
-        } else {
-            core.info("No need to update readme");
-        }
+        writeMD({ leaderBoardMd });
     } catch (error) {
-        core.setFailed("[run] " + error.message);
+        core.setFailed("[main] " + error.message);
         console.error(error);
     }
 }
 
-run();
+main();
 
