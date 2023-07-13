42b79cb402b94b614331e227380b5c005a56b375
wenshiu.hsu <wenshiu.hsu@gmail.com>
2023-07-13 17:25:04 +0800

    feat: 🎸 new input - IS_SKIPPING_WRITING_FILES

--- scripts/utils/index.js
+++ scripts/utils/index.js
@@ -1,10 +1,12 @@
 const markdown = require("./markdown.js");
 const json = require("./json.js");
 const dayJs = require("./dayjs.js");
+const envValidation = require("./envValidation.js");
 
 module.exports = {
     markdown,
     json,
     dayJs,
+    envValidation,
 };
 
