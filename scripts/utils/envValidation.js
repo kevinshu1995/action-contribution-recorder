24cd6a29e2304534397caba589ccb78a16ee5c92
wenshiu.hsu <wenshiu.hsu@gmail.com>
2023-07-13 17:25:43 +0800

    refactor: 💡 define reusable truthy value for envValidation fns

--- scripts/utils/envValidation.js
+++ scripts/utils/envValidation.js
@@ -1,9 +1,14 @@
 const core = require("@actions/core");
 
+const customTruthyArray = ["true", true, 1];
+const isTruthy = val => customTruthyArray.includes(val);
+
+const infoLogPrefix = "| 🟢 INFO |";
+
 function isSkippingWritingFiles(path) {
     const IS_SKIPPING_WRITING_FILES = core.getInput("IS_SKIPPING_WRITING_FILES");
-    if (IS_SKIPPING_WRITING_FILES === "true" || IS_SKIPPING_WRITING_FILES === true) {
-        core.info(`| 🟢 INFO | Skipping writing file \n\t file path: ${path} \n`);
+    if (isTruthy(IS_SKIPPING_WRITING_FILES)) {
+        core.info(`${infoLogPrefix} Skipping writing file \n\t file path: ${path} \n`);
         return true;
     }
     return false;
@@ -11,8 +16,8 @@ function isSkippingWritingFiles(path) {
 
 function isSkippingCommitting() {
     const IS_SKIPPING_COMMITTING = core.getInput("IS_SKIPPING_COMMITTING");
-    if (["true", true, 1].includes(IS_SKIPPING_COMMITTING)) {
-        core.info(`| 🟢 INFO | Skipping committing \n`);
+    if (isTruthy(IS_SKIPPING_COMMITTING)) {
+        core.info(`${infoLogPrefix} Skipping committing \n`);
         return true;
     }
     return false;
